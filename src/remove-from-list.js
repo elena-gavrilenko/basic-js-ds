49cd4de244a975fca9861462fd557c3cb4abde2f
Helena <gomel-offis@mail.ru>
2025-01-04 21:45:05 +0300

    feat: add one tasks

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  class ListNode {
+    constructor(x) {
+      this.value = x;
+      this.next = null;
+    }
+    removeAt(index){
+      if (index<0)
+    }
+  remove (elem){
+    this.removeAt(this.indexOf(elem))
+  }
+  }
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
